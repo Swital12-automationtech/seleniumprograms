6bd9350d99e5353d4f526a972cd5dcc9c07da089
Swital <switalmacwan2207@gmail.com>
2025-01-29 12:09:37 +0530

    update testcases all

--- src/main/java/packageofamazon/Project_Maven_Amazon/HomePage.java
+++ src/main/java/packageofamazon/Project_Maven_Amazon/HomePage.java
@@ -1,11 +1,16 @@
 package packageofamazon.Project_Maven_Amazon;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage 
 {
@@ -27,8 +32,11 @@ public class HomePage
 	}
 	public void hoverover(WebDriver driver)
 	{
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement continueButton = wait.until(ExpectedConditions.elementToBeClickable(accountandlist_hoverover));
+		//continueButton.click();
 		Actions a1 = new Actions(driver);
-		a1.moveToElement(accountandlist_hoverover).perform();
+		a1.moveToElement(continueButton).perform();
 	}
 	public void signin()
 	{
