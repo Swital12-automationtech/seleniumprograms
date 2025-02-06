cbc880d518b0d8d013b4844cbcc2194cbea5f06b
Swital <switalmacwan2207@gmail.com>
2025-02-06 16:16:53 +0530

    second commit2

--- src/test/java/packageofamazon/Project_Maven_Amazon/LaunchQuit.java
+++ src/test/java/packageofamazon/Project_Maven_Amazon/LaunchQuit.java
@@ -6,16 +6,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class LaunchQuit 
 {
-	EdgeDriver driver;
+	FirefoxDriver driver;
 	@BeforeMethod
 	public void launch()
 	{
-		driver = new EdgeDriver();
+		driver = new FirefoxDriver();
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
